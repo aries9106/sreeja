db1168a320e7ea32812c04c25df19facc69f003c
aries9106 <60824252+aries9106@users.noreply.github.com>
2020-02-10 12:39:04 -0600

    Add files via upload

--- Palindrome.js
+++ Palindrome.js
@@ -5,7 +5,7 @@
 
 
 console.log(PalindromeSeachReturn('civic'))
-
+/*** this method contains code to return an array of all the palindromes in a given value, if there are no palindromes then it return empty array***/
 function PalindromeSeachReturn(value){
   if(value.length > 1){
     var pal_val = [];
@@ -20,6 +20,8 @@ function PalindromeSeachReturn(value){
     return pal_val;
   }
   return 'Palindrome must be at least 2 characters long';
+  
+  /** method to check if given value is palindrome or **/
   function palindromeCheck(val){
     if(val.length > 1 && (val.split('').reverse().join('') == val)){
        return true;      
